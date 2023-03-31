15c001cb749a1cc12117b459341d37360de259ab
Diane Leigh <leighd2008@gmail.com>
2023-03-31 17:37:19 -0400

    had to move my features folder inside src

--- app/src/features/clients/ClientsList.jsx
+++ app/src/features/clients/ClientsList.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const ClientsList = () => {
+  const clients = useSelector(state => state.clients)
   
   return (
     <React.Fragment >
       <h2>Clients</h2>
+      {clients.map((client, i) => (
+        <h3 key={i}>{client.name}</h3>
+      ))}
     </React.Fragment>
   )
 }
