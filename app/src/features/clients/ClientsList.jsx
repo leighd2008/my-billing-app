7f7d3b83e5c4bef60e457a2de53773e9bd98adf3
Diane Leigh <leighd2008@gmail.com>
2023-04-13 13:52:43 -0400

    changed client list to table

--- app/src/features/clients/ClientsList.jsx
+++ app/src/features/clients/ClientsList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 import { fetchClients } from '../../redux/components/Clients/clients.actions'
@@ -8,65 +7,39 @@ import { fetchClients } from '../../redux/components/Clients/clients.actions'
 export const ClientsList = () => {
   const dispatch = useDispatch();
   const clients = useSelector(state => state.clients)
-  // debugger
+  const navigate = useNavigate()
   
   useEffect(() => {
     dispatch(fetchClients())
   }, [dispatch])
   
-  // const renderClients = clients.map(client => (
-  //   <div key={client.id}>
-  //     <h3>{`${client.firstName} ${client.lastName}`}</h3>
-  //     <Link to={`/clients/${client.id}`}>View Client</Link>
-  //   </div>
-  // ))
-  
-  const navigate = useNavigate()
-  
   return (
-    <div>
-      <h2>Clients</h2>
-      <ul>
-        {
-          clients.map(client =>
-          <li
-            key={client.id}
-            onClick={() => navigate(`/clients/${client.id}`)}
-          >
-            <h3>{`${client.firstName} ${client.lastName}`}</h3>
-          </li>
-          )
-        }
-      </ul>
-      {/* {renderClients} */}
-    </div>
+    <React.Fragment >
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client, i) => {
+            return (
+              <tr key={i+1}>
+                <td>{`${client.firstName} ${client.lastName}`}</td>
+                <td>{client.address}</td>
+                <td>{client.city}</td>
+                <td>{client.state}</td>
+                <td>{client.zip}</td>
+                <td><button onClick={() => navigate(`/clients/${client.id}`)}>View Client</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
   )
-  // return (
-  //   <React.Fragment >
-  //     <table>
-  //       <thead>
-  //         <tr>
-  //           <th>Name</th>
-  //           <th>Address</th>
-  //           <th>City</th>
-  //           <th>State</th>
-  //           <th>Zip Code</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {clients.map((client, i) => {
-  //           return (
-  //             <tr key={i+1}>
-  //               <td>{`${client.firstName} ${client.lastName}`}</td>
-  //               <td>{client.address}</td>
-  //               <td>{client.city}</td>
-  //               <td>{client.state}</td>
-  //               <td>{client.zip}</td>
-  //             </tr>
-  //           )
-  //         })}
-  //       </tbody>
-  //     </table>
-  //   </React.Fragment>
-  // )
 }
