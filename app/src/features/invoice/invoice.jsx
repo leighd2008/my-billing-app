9cedab2fe65cb5e8ea3d62945d400393cd7e9d29
Diane Leigh <leighd2008@gmail.com>
2023-06-29 08:20:03 -0400

    added '@react-pdf/renderer' and generate-dynamic-pdf-invoice

--- app/src/features/invoice/invoice.jsx
+++ app/src/features/invoice/invoice.jsx
@@ -1,20 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import { Page, Document, Image, StyleSheet } from '@react-pdf/renderer';
+import InvoiceTitle from './InvoiceTitle';
+import BillTo from './BillTo';
+import InvoiceNo from './InvoiceNo';
+import InvoiceItemsTable from './InvoiceItemsTable';
+import InvoiceThankYouMsg from './InvoiceThankYouMsg';
+// import logo from '../../../src/logo.svg';
+import invoice from './data/invoice-data';
 
-export default class Invoice extends React.Component {
-  render() {
-    return (
-    <React.Fragment>
-      <section className="section">
-        <div className="centered-view">
-          <section className="centered-container">
-            <div className="">
-              <p className="title">Generate an invoice</p>
-            </div>
-          </section>
-        </div>
-      </section>
-    </React.Fragment>
-    );
+const styles = StyleSheet.create({
+  page: {
+    fontFamily: 'Helvetica',
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    lineHeight: 1.5,
+    flexDirection: 'column',
+  },
+  logo: {
+    width: 74,
+    height: 68,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   }
+});
+
+const Invoice = () => {
+  
+  return (
+    <Fragment>
+        <PDFViewer width="1000" height="600" className="app">
+          <Document>
+            <Page size="A4" style={styles.page}>
+              {/* <Image style={styles.logo} src={logo} /> */}
+              <InvoiceTitle title="Invoice" />
+              <InvoiceNo invoice={invoice} />
+              <BillTo invoice={invoice} />
+              <InvoiceItemsTable invoice={invoice} />
+              <InvoiceThankYouMsg />
+            </Page>
+          </Document>
+        </PDFViewer>
+      </Fragment>
+  );
 }
+
+export default Invoice;
